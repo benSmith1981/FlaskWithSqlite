a76d02d1c3663f2a96ee9b908fe66dc2b33c87f7
Ben Smith <benjaminsmith1981@googlemail.com>
2023-06-13 09:51:20 +0000

    added a contact page to

--- app.py
+++ app.py
@@ -6,14 +6,24 @@ app = Flask(__name__)
 def init_db():
     conn = sqlite3.connect('contact.db')
     cursor = conn.cursor()
-    cursor.execute("""
-    CREATE TABLE contact (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        email TEXT NOT NULL,
-        message TEXT NOT NULL
-    );
-    """)
+
+    # Check if the table already exists
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='contact'")
+    table_exists = cursor.fetchone()
+
+    if table_exists:
+        print("Table 'contact' already exists.")
+    else:
+        cursor.execute("""
+        CREATE TABLE contact (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            message TEXT NOT NULL
+        );
+        """)
+        print("Table 'contact' created.")
+
     conn.commit()
     conn.close()
 
@@ -34,6 +44,17 @@ def index():
 
     return render_template('index.html')
 
+
+@app.route('/contacts', methods=['GET'])
+def view_contacts():
+    conn = sqlite3.connect('contact.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM contact")
+    contacts = cursor.fetchall()
+    conn.close()
+
+    return render_template('contacts.html', contacts=contacts)
+    
 if __name__ == "__main__":
     init_db()
     app.run(debug=True)
