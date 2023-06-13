1010f3a1ca802c442c942914ee57b0f574dbed08
Ben Smith <benjaminsmith1981@googlemail.com>
2023-06-13 11:39:30 +0000

    added login page

--- app.py
+++ app.py
@@ -44,6 +44,9 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 @app.route('/contacts', methods=['GET'])
 def view_contacts():
